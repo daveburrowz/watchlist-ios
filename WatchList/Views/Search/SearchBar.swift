c1cd82252c90d92d5189875abeadc2b1b903176d
David Burrows <daveburrowz@gmail.com>
2020-06-28 17:43:35 +0100

    Changed default text

--- WatchList/Views/Search/SearchBar.swift
+++ WatchList/Views/Search/SearchBar.swift
@@ -16,7 +16,7 @@ struct SearchBar: View {
     var body: some View {
         HStack {
             
-            TextField("Search ...", text: $text)
+            TextField("Search", text: $text)
                 .padding(7)
                 .padding(.horizontal, 25)
                 .background(Color(.systemGray6))
